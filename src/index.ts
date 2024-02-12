0763cef44cb4d5817c14a2c7e93b4e6444f973c9
Samarth Verma <samarth.verma@stationcasinos.com>
2024-02-12 17:07:11 +0530

    adding trpc code

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+export type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+
+export const appRouter = t.router({
+  hello: t.procedure.query(async () => {
+    const message = "Hello Guys!!";
+    return { message };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
